27fce4da5a65100cf53538770553ceec59899388
Krislv4nk <kristinalev4enko89@gmail.com>
2024-05-13 11:18:43 +0200

    test config

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
      
-    <BrowserRouter basename="/Camper-club">
+    <BrowserRouter basename="/CamperClub">
       <App /><ToastContainer
             position="top-center"
             autoClose={2000}
